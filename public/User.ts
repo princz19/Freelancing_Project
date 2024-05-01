c4f22c2b4b3b632540e77b8f041000d0f4d080d0
princz19 <princeji4562@gmail.com>
2024-05-01 15:18:10 +0530

    push

--- public/User.ts
+++ public/User.ts
@@ -17,3 +17,63 @@ const MessageSchema: Schema<Message> = new Schema({
         default : Date.now
     }
 })
+
+
+export interface User extends Document{
+  username: string;
+  email: string;
+  password : string;
+  verifyCode: string;
+  verifyCodeExpiry: Date;
+  isVerifed : boolean;
+  isAcceptingMessage: boolean;
+  messages: Message[]
+
+}
+
+const UserSchema: Schema<Message> = new Schema({
+username: {
+    type: String,
+    required : [true,"Username is required"],
+    trim: true,
+    unique: true
+
+},
+email: {
+    type: String,
+    required : [true,"Email is required"],
+    unique :true,
+    match: [/.+\@.+\..+/, 'please use a valid email address']
+
+},
+   password : {
+    type: String,
+    required : [true,"Password  is required"],
+    },
+ verifyCode:{
+    type: String,
+    required :[true,"VerifyCode is required"],
+
+ },
+ verifyCodeExpiry: {
+    type: Date,
+    required :[true,"VerifyCode Expiry is Required"],
+
+ },
+ isVerifed:{
+    type: Boolean,
+    default : false,
+
+ },
+ isAcceptingMessage:{
+    type: Boolean,
+    default: true,
+ },
+ messages: [MessageSchema]
+
+})
+
+const UserModel = (mongoose.models.User as mongoose.
+Model<User>) || mongoose.model<User>("User",UserSchema)
+
+export default UserModel;
